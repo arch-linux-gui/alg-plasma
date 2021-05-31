6ae962cddd9b43f8defe6ca3991351057171d95c
demonkiller2 <hiranyagarbh7@gmail.com>
2021-05-31 23:39:02 +0530

    updated profile def

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux-gui"
+iso_name="archlinux-gui-plasma"
 iso_label="ARCH_GUI_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
