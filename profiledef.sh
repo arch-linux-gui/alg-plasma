e2120000945a154c1c4540770281ac5e21e471b9
demonkiller <gaurangvishwakarma619@gmail.com>
2021-10-01 03:59:02 +0530

    releng 21'10

--- profiledef.sh
+++ profiledef.sh
@@ -10,6 +10,7 @@ install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -20,5 +21,5 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/alg-remove-nvidia"]="0:0:755"
   ["/usr/local/bin/alg-preset"]="0:0:755"
-  ["/usr/local/bin/alg-finalisation"]="0:0:755"	
+  ["/usr/local/bin/alg-finalisation"]="0:0:755"
 )
