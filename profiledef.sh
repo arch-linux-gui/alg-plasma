b2dad6244faea113de0a58afaf8b29b25162012c
demonkiller <gaurangvishwakarma619@gmail.com>
2021-10-01 03:34:06 +0530

    added scripts and update profile

--- profiledef.sh
+++ profiledef.sh
@@ -18,5 +18,7 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/remove-nvidia"]="0:0:755"	
+  ["/usr/local/bin/alg-remove-nvidia"]="0:0:755"
+  ["/usr/local/bin/alg-preset"]="0:0:755"
+  ["/usr/local/bin/alg-finalisation"]="0:0:755"	
 )
